8b8d1fde2840cb1b4381504fe81a4917a670e339
BananaNeil <neil@countable.us>
2018-11-17 13:26:54 +0000

    Making sure createPreview finds the correct file and doesn't need confirmation to overwrite it

--- createPreview.sh
+++ createPreview.sh
@@ -1,5 +1,6 @@
-project=`ls ~/artistic-videos/inProgress -t | head -1`
+project=`ls ~/artistic-videos/inProgress -t | grep -v laststate.txt | head -1`
 dir=~/artistic-videos/inProgress/${project}
 outDir=$dir/`ls $dir/ -t | grep ${project} | head -1`
 echo $outDir
+rm /tmp/preview.mov
 ffmpeg -i $outDir/out-%04d.png -loglevel 'error'  -pix_fmt yuv420p -framerate 30 /tmp/preview.mov
