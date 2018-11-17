9ba5797d6a7b85d8a56f4226c4e094b40a815e15
BananaNeil <neil@countable.us>
2018-11-17 13:46:49 +0000

    Added first and last frames to the preview script

--- createPreview.sh
+++ createPreview.sh
@@ -1,6 +1,12 @@
 project=`ls ~/artistic-videos/inProgress -t | grep -v laststate.txt | head -1`
 dir=~/artistic-videos/inProgress/${project}
 outDir=$dir/`ls $dir/ -t | grep ${project} | head -1`
+totalFrames=$(exec ls $dir | grep frame | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)
+currentFrame=$(exec ls $outDir | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)
+
 echo $outDir
+echo "Exported $currentFrame/$totalFrames frames"
+
+
 rm /tmp/preview.mov
 ffmpeg -i $outDir/out-%04d.png -loglevel 'error'  -pix_fmt yuv420p -framerate 30 /tmp/preview.mov
