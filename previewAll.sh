93ad3e872968591595323842effee9b2737f0426
BananaNeil <neil@countable.us>
2018-11-18 13:39:47 -1000

    fixed a typo

--- previewAll.sh
+++ previewAll.sh
@@ -9,7 +9,7 @@ rm -r /tmp/previews/preview-*.mov
 i=1
 for outDir in $outfiles; do
   echo $outDir
-  frames =$(exec ls $outDir | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)
+  frames=$(exec ls $outDir | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)
   echo "preview-$i frames: $frames"
   ffmpeg -i $outDir/out-%04d.png -loglevel 'error'  -pix_fmt yuv420p -framerate 30 /tmp/previews/preview-$i-$frames.mov
   i=$(($i+1))
