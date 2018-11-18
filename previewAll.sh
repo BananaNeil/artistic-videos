521abab33db1ee65a3d9c4546ded024376b3b38d
BananaNeil <neil@countable.us>
2018-11-18 13:38:42 -1000

    previewAll script removes old previews and attaches frame count to file names

--- previewAll.sh
+++ previewAll.sh
@@ -5,11 +5,13 @@ outfiles=$(find ~/artistic-videos/inProgress/ -name 'out*.png' | sed 's/[a-z0-9\
 
 
 mkdir /tmp/previews
+rm -r /tmp/previews/preview-*.mov
 i=1
 for outDir in $outfiles; do
   echo $outDir
-  echo "preview-$i frames: $(exec ls $outDir | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)"
-  ffmpeg -i $outDir/out-%04d.png -loglevel 'error'  -pix_fmt yuv420p -framerate 30 /tmp/previews/preview-$i.mov
+  frames =$(exec ls $outDir | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)
+  echo "preview-$i frames: $frames"
+  ffmpeg -i $outDir/out-%04d.png -loglevel 'error'  -pix_fmt yuv420p -framerate 30 /tmp/previews/preview-$i-$frames.mov
   i=$(($i+1))
 done
 tar -cvjf /tmp/previews.tar.gz /tmp/previews
