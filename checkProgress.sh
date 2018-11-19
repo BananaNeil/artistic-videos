ef42cf42d243ce6d83658343655e42c21a6c1805
BananaNeil <neil@countable.us>
2018-11-19 09:11:14 -1000

    Added check progress script

---
+++ checkProgress.sh
@@ -0,0 +1,8 @@
+project=`ls ~/artistic-videos/inProgress -t | grep -v laststate.txt | head -1`
+dir=~/artistic-videos/inProgress/${project}
+outDir=$dir/`ls $dir/ -t | grep ${project} | head -1`
+totalFrames=$(exec ls $dir | grep frame | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)
+currentFrame=$(exec ls $outDir | sed 's/[^0-9]*\([0-9]\+\).*/\1/g'  | sort -n | tail -1)
+
+echo $outDir
+echo "Exported $currentFrame/$totalFrames frames"
